84e04a7312f2d0458247421f238782f8b8b3f554
Emmanuel <peixoto.2078@gmail.com>
2024-06-01 20:58:47 +0000

    push

---
+++ scripts/deploy.sh
@@ -0,0 +1,5 @@
+#Transferir Arquivos: Copiar arquivos do repositório para o ambiente de produção.
+#Configurar o Ambiente: Configurar variáveis de ambiente, permissões, etc.
+#Instalar Dependências: Instalar qualquer dependência necessária.
+#Iniciar/Reiniciar Serviços: Iniciar ou reiniciar serviços da aplicação.
+#Realizar Tarefas Pós-Implantação: Limpar caches, migrar bancos de dados, etc.
---
+++ scripts/start.sh
@@ -0,0 +1,7 @@
+#Iniciar um servidor web (ex: node server.js para um projeto Node.js).
+#Compilar código (ex: npm run build para um projeto JavaScript).
+#Iniciar serviços auxiliares (ex: iniciar um banco de dados ou uma fila de mensagens).
+#Configurar logs ou diretórios temporários.
+#Iniciar a Aplicação: Executar o comando ou série de comandos necessários para iniciar o aplicativo.
+#Configurar o Ambiente: Configurar variáveis de ambiente ou outras dependências necessárias antes de iniciar o aplicativo.
+#Preparar Dependências: Instalar ou verificar dependências que o projeto necessita antes de iniciar.
